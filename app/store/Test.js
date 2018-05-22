20956410ac9dc24cf281bdb0558aa00ce3e61c91
Denis <catstrategist@rambler.ru>
2018-05-23 00:21:45 +0300

    Added cors

--- app/store/Test.js
+++ app/store/Test.js
@@ -4,24 +4,20 @@ Ext.define('Tf.store.Test', {
     ],
     extend: 'Ext.data.Store',
     alias: 'store.Test',
-
+    autoLoad: true,
+    autoSync: true,
+    cors: true,
     storeId: 'test',
     model: 'Tf.model.Test',
-    // proxy: {
-    //     type: 'rest',
-    //     url: 'http://localhost:8080/status',
-    //     reader: {
-    //         type: 'json',
-    //         root: 'data'
-    //     },
-    //     writer: {
-    //         type: 'json'
-    //     }
-    // },
-    // fields: ['statusId', 'statusName'],
-    data: [
-        {statusId: '1', statusName: 'Active'},
-        {statusId: '2', statusName: 'Passive'},
-    ],
-
+    proxy: {
+        type: 'rest',
+        url: 'http://localhost:8080/status',
+        reader: {
+            type: 'json',
+            rootProperty: 'data'
+        },
+        writer: {
+            type: 'json'
+        }
+    },
 });
