eec2dafddefe3a0970ac193edd09dee9b12ec8a2
Vlad <templerock@mail.ru>
2018-05-30 02:05:44 +0300

    container layout vbox center

--- app/view/login/Login.js
+++ app/view/login/Login.js
@@ -1,71 +1,68 @@
 Ext.define('Tf.view.login.Login', {
   extend: 'Ext.Panel',
     controller: 'main',
-    style: {margin: '100px 0 0 750px', display: 'inline-block'},
-    bodyPadding: 50,
     requires: [
         'Ext.Button',
         'Ext.form.*'
     ],
-
-  xtype: 'login',
-  id: 'loginWindow',
-  // title: 'Login page',
+    xtype: 'login',
+    id: 'loginWindow',
+    // title: 'Login page',
     title: 'Tire-fitting',
-  viewModel: {
-    data: {
-      login: '',
-      password: '',
-    }
-  },
+    viewModel: {
+        data: {
+            login: '',
+            password: '',
+        }},
+                xtype : 'container',
+                    layout: {
+                        type: 'vbox',
+                        pack: 'start',
+                        align: 'center'
+                    },
 
-  items: [
-      {
-          xtype: 'image',
-          src: '/app/img/arg.png',
-          margin: '0 0 0 -250px',
-          width: 800,
-          height: 500,
-      },
-      {
-      xtype: 'textfield',
-      label: 'Login',
-      bind: '{login}',
-      fieldLabel: 'Login',
-      reference: 'logLogin',
-  }, {
-      xtype: 'textfield',
-          bodyPadding: 50,
-      label: 'Password',
-      inputType: 'password',
-      bind: '{password}',
-      listeners: {
-          action: 'onLogin'
-      },
-      fieldLabel: 'Password',
-      reference: 'logPass'
-  }, {
-      xtype: 'button',
-      // x:0,
-      // y:0,
-          margin: '10px 0 0 0',
-      width:274,
-      height:50,
-      handler: 'onLogin',
-      bind: {
-          disabled: '{!password||!login}',
-          text: 'Login'
-      }
-  },
-      {
-          xtype: 'button',
-          bodyPadding: 50,
-          // x:-270,
-          // y:80,
-          margin: '80px 0 0 -274px',
-          width:274,
-          height:50,
-          handler: 'onRegisterClick',
-          text: 'Registration'
-      }]
+                    margin: '120px 10px 10px 10px',
+                    items: [
+                    {
+                        xtype: 'image',
+                        src: '/app/img/arg.png',
+                        width: 700,
+                        height: 400,
+                    },
+                    {
+                        xtype: 'textfield',
+                        padding: '10px 50px 10px 50px',
+                        label: 'Login',
+                        bind: '{login}',
+                        fieldLabel: 'Login',
+                        reference: 'logLogin',
+                    }, {
+                        xtype: 'textfield',
+                            padding: '10px 50px 10px 50px',
+                        label: 'Password',
+                        inputType: 'password',
+                        bind: '{password}',
+                        listeners: {
+                            action: 'onLogin'
+                        },
+                        fieldLabel: 'Password',
+                        reference: 'logPass'
+                    }, {
+                        xtype: 'button',
+                            width: 276,
+                            align: 'stretch',
+                            margin: '10px 10px 10px 10px',
+                        handler: 'onLogin',
+                        bind: {
+                            disabled: '{!password||!login}',
+                            text: 'Login'
+                        }
+                    },
+                    {xtype: 'button',
+                        width: 276,
+                        align: 'stretch',
+                        margin: '10px 10px 10px 10px',
+                        handler: 'onRegisterClick',
+                        text: 'Registration'
+                    }]
 });
